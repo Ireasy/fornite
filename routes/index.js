93e32f0a8613f967705bc3330a63fda49a02e21d
Per Bernhardt <per.bernhardt@leanix.net>
2024-04-19 11:06:02 +0200

    Seite umgebaut mit erste Seite für Freunde

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const freunde = ['Per', 'Tim', 'Peter', 'Max'];
+
+  res.render('index', {
+     freunde: freunde
+  });
 });
 
 module.exports = router;
