269a54fb8c538ec8531830a208a6624eb65af945
Tim Gaida <timoncam77@gmail.com>
2024-04-22 10:55:10 +0200

    Deutsches Datum

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var _ = require('lodash')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  const freunde = ['Per', 'Tim', 'Peter', 'Max'];
+  const freunde = ['Johannes', 'Tim', 'Peter', 'Max'];
 
   res.render('index', {
      freunde: freunde,
@@ -16,29 +16,7 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 function timeCount() {
   var today = new Date();
-
-  var day = today.getDate();
-  var month = today.getMonth()+1;
-  var year = today.getFullYear();
-
-  var hour = today.getHours();
-  if(hour<10)hour = "0"+hour;
-
-  var minute = today.getMinutes();
-  if(minute<10)minute = "0"+minute;
-
-  var second = today.getSeconds();
-  if(second<10)second = "0"+second;
-
-  Calendar calendar = new GregorianCalendar();
-TimeZone timeZone = calendar.getTimeZone();
-
-
-
-
-  return day+"/"+month+"/"+year+" |"+hour+":"+minute+":"+second;
-
-
+  return today.toLocaleString('de-DE', { timeZone : 'Europe/Berlin'});
 
   
 }
